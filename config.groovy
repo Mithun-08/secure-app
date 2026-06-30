cdcf62186a8346590e6b6f15feeed8bc3dcd2884
Mithun Prabhu R <imithunprabhu@gmail.com>
2026-06-30 22:31:05 +0530

    Change GCP signer identity in config.groovy
    Updated GCP signer identity for configuration.

--- config.groovy
+++ config.groovy
@@ -3,7 +3,7 @@ return [
     imageName           : 'mithun-08/hello-java',
     ghcrCredentialsId   : 'ghcr-credentials',
     gcpKeyCredentialsId : 'gcp-signer-key',
-    gcpSignerIdentity   : 'signer@stackbill-supply-chain-1842.iam.gserviceaccount.com',
+    gcpSignerIdentity   : 'jenkins-signer@stackbill-supply-chain-1842.iam.gserviceaccount.com',
     oidcIssuer          : 'https://accounts.google.com',
     k3sNamespace        : 'default',
     k3sDeploymentName   : 'secure-java-payload',
